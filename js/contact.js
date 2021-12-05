f952a34471cfcd7424959e332522b43eb637a6ae
tronderic <73131969+tronderic@users.noreply.github.com>
2021-12-05 14:02:44 +0100

    worked on form validation and form css

---
+++ js/contact.js
@@ -0,0 +1,49 @@
+const form = document.querySelector("#form");
+const yourName = document.querySelector("#name");
+const nameError = document.querySelector("#nameError");
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#messageError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const successMessage = document.querySelector(".successMessage");
+
+function validateForm(event) {
+    event.preventDefault();
+
+    let validationPassed = true;
+
+    if (yourName.value.trim().length > 0) {
+        nameError.style.display = "none";
+    } else {
+        nameError.style.display = "block";
+        validationPassed = false;
+    }
+
+    if (validateEmail(email.value)) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+        validationPassed = false;
+    }
+
+    if (message.value.trim().length > 0) {
+        messageError.style.display = "none";
+    } else {
+        messageError.style.display = "block";
+        validationPassed = false;
+    }
+
+    if (validationPassed) {
+        successMessage.innerHTML = "<div id='successMessage'>Your request has been submitted</div>";
+    } else {
+        successMessage.innerHTML = "";
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
+
+form.addEventListener("submit", validateForm);
