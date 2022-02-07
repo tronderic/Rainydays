572c81211f151a150efdff00599aebd2ed2c8591
tronderic <73131969+tronderic@users.noreply.github.com>
2022-02-07 17:34:14 +0100

    fetch wp API featured products

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const baseUrl = "https://rainydays.aadnoywebdev.one/wp-json/wc/store/products?featured=true";
+const container = document.querySelector(".popular-items");
+
+async function retrieveProducts(url) {
+    const data = await fetch(url);
+    const products = await data.json();
+
+    console.log(products[0]);
+
+    products.forEach((product) => {
+        container.innerHTML += `
+        <div class="product">
+          <img src="${product.images[0].src}" alt="Jacket" />
+          <h3>${product.name}</h3>
+          <p>${product.price_html}</p>
+          <a href="product-page.html" class="cta">View</a>
+        </div>`;
+    });
+}
+
+retrieveProducts(baseUrl);
