b26b8f57f3e17375e80c22068d9c9c98020c8ee1
tronderic <73131969+tronderic@users.noreply.github.com>
2022-02-07 17:10:07 +0100

    added fetch API from WP

---
+++ js/products.js
@@ -0,0 +1,21 @@
+const baseUrl = "https://rainydays.aadnoywebdev.one/wp-json/wc/store/products?per_page=15";
+const container = document.querySelector(".products");
+
+async function retrieveProducts(url) {
+    const data = await fetch(url);
+    const products = await data.json();
+
+    console.log(products[0]);
+
+    products.forEach((product) => {
+        container.innerHTML += `
+        <div class="product">
+          <img src="${product.images[0].src}" alt="Jacket" />
+          <h3>${product.name}</h3>
+          <p>${product.price_html}</p>
+          <a href="product-page.html" class="cta">View</a>
+        </div>`;
+    });
+}
+
+retrieveProducts(baseUrl);
